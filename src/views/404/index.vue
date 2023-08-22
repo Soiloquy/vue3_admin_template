ca6caca99356e34553a88b45adb243b14d98fd98
Soiloquy <yushantang05@gmail.com>
2023-08-22 22:35:14 +0800

    404页面

--- src/views/404/index.vue
+++ src/views/404/index.vue
@@ -1,12 +1,17 @@
 <template>
-    <div>
-        <h1>404</h1>
+    <div class="title">
+        404
     </div>
 </template>
 
 <script lang="ts" setup>
 </script>
 
-<style scoped>
-
+<style scoped lang="scss">
+.title{
+    display: flex;
+    justify-content: center;
+    font-size: 68px;
+    font-weight: 900;
+}
 </style>
